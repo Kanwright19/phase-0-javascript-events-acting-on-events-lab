93f86984a0220af53840c69773196a520bdfe947
Kairi Wright <kanwright19@gmail.com>
2023-12-07 18:13:39 -0500

    move dodger around

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  //
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  //
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+  
+  function moveDodgerRight() {
+    const redNumber = dodger.style.left.replace("px","")
+    const red = parseInt(redNumber, 10);
+
+    if (red > 0) {
+        dodger.style.left = `${red + 1}px`;
+    }
+  }
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+  });
